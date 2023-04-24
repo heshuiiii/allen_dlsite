4696fda407cf19eef73a9d59e352362d5444c154
heshuiiii <71701401+heshuiiii@users.noreply.github.com>
2023-04-24 20:18:19 +0800

    Update Dlsite.php

--- src/Dlsite.php
+++ src/Dlsite.php
@@ -1,11 +1,19 @@
 <?php
+
 namespace allen\dlsite;
 
+class Dlsite {
+  public static function getWorkDetails($work_id) {
+    $url = 'https://www.dlsite.com/maniax/work/=/product_id/' . $work_id . '.html';
+    $html = file_get_contents($url);
 
-class Dlsite
-{
-    public function getWorkDetails($work_id)
-    {
-        return $work_name , $details;
-    }
+    // 获取指定元素
+    $dom = new DOMDocument();
+    @$dom->loadHTML($html);
+    $xpath = new DOMXPath($dom);
+    $work_name = $xpath->query('//h1[@class="work_name"]/text()')->item(0)->nodeValue;
+    $details = $xpath->query('//div[@class="work_story"]/p/text()')->item(0)->nodeValue;
+
+    return array("work_name" => $work_name, "details" => $details);
+  }
 }
